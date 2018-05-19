e38c219d89e834da9843320d3acc12242ad08672
Daniel Neil Darling <danieldarling94@gmail.com>
2018-05-19 15:26:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var x = 1,
+    y = 80,
+    a = 60,
+    b = 40,
+    c = 2,
+    d = 3.4,
+    e = 5.0,
+    f = 2.5;
+
+function add(x,y){
+  return x + y;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(c,d){
+  return c * d;
+}
+
+function divide(e,f){
+  return e / f;
+}
+
+var n = 5;
+++n;
+console.log (n == 5);
+  
